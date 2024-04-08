e7734466034526c2b01f3e8bd9c33a65c98a3b5a
Weihao Feng <>
2024-04-08 20:06:40 +0200

    Revert the changes...

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -45,7 +45,6 @@ return {
         local stat = vim.uv.fs_stat(vim.fn.argv(0))
         if stat and stat.type == "directory" then
           require("neo-tree")
-          vim.cmd("Neotree toggle")
         end
       end
     end,
